d4d9a48b034dc5369189cbffff45dbdc818810b7
dieatmore <s.d.wang.1@nefu.edu.cn>
2025-12-24 14:07:48 +0800

    自定义回复模板

--- src/main/java/org/example/raglearn/service/HandbookService.java
+++ src/main/java/org/example/raglearn/service/HandbookService.java
@@ -4,11 +4,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.template.st.StTemplateRenderer;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+import java.util.Map;
 import java.util.stream.Collectors;
 @Service
 @Slf4j
@@ -18,12 +21,36 @@ public class HandbookService {
     private final VectorStore vectorStore;
 
     public String getAnswer(String question) {
+
+        // 构建自定义Prompt模板
+        PromptTemplate customPromptTemplate = PromptTemplate.builder()
+                .renderer(StTemplateRenderer.builder().startDelimiterToken('<').endDelimiterToken('>').build())
+                .template("""
+                    <query>
+
+                    Context information is below.
+
+                    ---------------------
+                    <question_answer_context>
+                    ---------------------
+
+                    Given the context information and no prior knowledge, answer the query.
+
+                    Follow these rules:
+
+                    1. If the answer is not in the context, just say that you don't know.
+                    2. Avoid statements like "Based on the context..." or "The provided information...".
+                    """)
+                .build();
+
         var qaAdvisor = QuestionAnswerAdvisor.builder(vectorStore)
+                .promptTemplate(customPromptTemplate)
                 .searchRequest(SearchRequest.builder()
                         .similarityThreshold(0.4)
                         .topK(3)
                         .build())
                 .build();
+
         return chatClient.prompt()
                 .advisors(qaAdvisor)
                 .user(question)
--- src/test/java/org/example/raglearn/service/HandbookServiceTest.java
+++ src/test/java/org/example/raglearn/service/HandbookServiceTest.java
@@ -19,7 +19,9 @@ class HandbookServiceTest {
         // var str = "公司的年假有几天？";
         // var str = "可以请几天病假呢？";
         // var str = "我有张发票已经搁置40天了，还能报销么？";
-        var str = "请问，有多长时间的午休时间？";
+//        var str = "请问，有多长时间的午休时间？";
+        var str = "我的工资是多少？";
+
 
         var answer = handbookService.getAnswer(str);
         log.debug(answer);
